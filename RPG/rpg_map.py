e9d2999934259eaea7036b46af714e717a0ea2f3
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2021-02-21 17:19:51 +0900

    2021.02.21 update

--- RPG/rpg_map.py
+++ RPG/rpg_map.py
@@ -12,6 +12,9 @@ import pygame.locals
 import sys
 import rpg_define as idef
 
+#----------------------------
+# constant value
+#----------------------------
 DIR_L: int = -1
 DIR_R: int = -2
 DIR_U: int = 1
@@ -28,21 +31,129 @@ frontImg = [pygame.image.load("img/player/front/C-13.PNG"),
             pygame.image.load("img/player/front/C-4.PNG"),
             pygame.image.load("img/player/front/C-13.PNG"),
             pygame.image.load("img/player/front/C-2.PNG")]
-
 sideImg = [pygame.image.load("img/player/side/Cs-1.PNG"),
            pygame.image.load("img/player/side/Cs-2.PNG"),
            pygame.image.load("img/player/side/Cs-3.PNG"),
            pygame.image.load("img/player/side/Cs-4.PNG"),
            pygame.image.load("img/player/side/Cs-3.PNG"),
            pygame.image.load("img/player/side/Cs-2.PNG")]
+mapImg = [None,
+          pygame.image.load("img/map/t01.PNG"),
+          pygame.image.load("img/map/t02.PNG"),
+          pygame.image.load("img/map/t03.PNG"),
+          pygame.image.load("img/map/t04.PNG"),
+          pygame.image.load("img/map/t05.PNG"),
+          pygame.image.load("img/map/t06.PNG"),
+          pygame.image.load("img/map/t07.PNG"),
+          pygame.image.load("img/map/t08.PNG"),
+          pygame.image.load("img/map/t09.PNG"),
+          pygame.image.load("img/map/t10.PNG"),
+          pygame.image.load("img/map/t11.PNG"),
+          pygame.image.load("img/map/t12.PNG"),
+          pygame.image.load("img/map/t13.PNG"),
+          pygame.image.load("img/map/t14.PNG"),
+          pygame.image.load("img/map/t15.PNG"),
+          pygame.image.load("img/map/t16.PNG"),
+          pygame.image.load("img/map/t17.PNG"),
+          pygame.image.load("img/map/t18.PNG"),
+          pygame.image.load("img/map/t19.PNG"),
+          pygame.image.load("img/map/t20.PNG"),
+          pygame.image.load("img/map/t21.PNG"),
+          pygame.image.load("img/map/t22.PNG"),
+          pygame.image.load("img/map/t23.PNG"),
+          pygame.image.load("img/map/t24.PNG"),
+          pygame.image.load("img/map/t25.PNG"),
+          pygame.image.load("img/map/t26.PNG"),
+          pygame.image.load("img/map/t27.PNG"),
+          pygame.image.load("img/map/t28.PNG"),
+          pygame.image.load("img/map/t29.PNG"),
+          pygame.image.load("img/map/t30.PNG"),
+          pygame.image.load("img/map/t31.PNG"),
+          pygame.image.load("img/map/t32.PNG"),
+          pygame.image.load("img/map/t33.PNG"),
+          pygame.image.load("img/map/t34.PNG"),
+          pygame.image.load("img/map/t35.PNG"),
+          pygame.image.load("img/map/t36.PNG"),
+          pygame.image.load("img/map/t37.PNG"),
+          pygame.image.load("img/map/t38.PNG"),
+          pygame.image.load("img/map/t39.PNG"),
+          pygame.image.load("img/map/t40.PNG"),
+          pygame.image.load("img/map/t41.PNG"),
+          pygame.image.load("img/map/t42.PNG"),
+          pygame.image.load("img/map/t43.PNG"),
+          pygame.image.load("img/map/t44.PNG"),
+          pygame.image.load("img/map/t45.PNG"),
+          pygame.image.load("img/map/t46.PNG"),
+          pygame.image.load("img/map/t47.PNG"),
+          pygame.image.load("img/map/t48.PNG"),
+          pygame.image.load("img/map/t49.PNG"),
+          pygame.image.load("img/map/t50.PNG"),
+          pygame.image.load("img/map/t51.PNG"),
+          pygame.image.load("img/map/t52.PNG"),
+          pygame.image.load("img/map/t53.PNG")]
+overMapImg = [None,
+              pygame.image.load("img/map/a01.PNG"),
+              pygame.image.load("img/map/a02.PNG"),
+              pygame.image.load("img/map/a03.PNG"),
+              pygame.image.load("img/map/a04.PNG"),
+              pygame.image.load("img/map/a05.PNG"),
+              pygame.image.load("img/map/a06.PNG"),
+              pygame.image.load("img/map/a07.PNG"),
+              pygame.image.load("img/map/a08.PNG"),
+              pygame.image.load("img/map/a09.PNG"),
+              pygame.image.load("img/map/a10.PNG"),
+              pygame.image.load("img/map/a11.PNG"),
+              pygame.image.load("img/map/a12.PNG"),
+              pygame.image.load("img/map/a13.PNG"),
+              pygame.image.load("img/map/a14.PNG"),
+              pygame.image.load("img/map/a15.PNG"),
+              pygame.image.load("img/map/a16.PNG"),
+              pygame.image.load("img/map/a17.PNG"),
+              pygame.image.load("img/map/a18.PNG"),
+              pygame.image.load("img/map/a19.PNG")]
+
+nowMap = [[]]
 
 posX: int = 0
 posY: int = 0
 
 dir: int = 0
 
-def MapDraw(bg):
-    a=0
+def MapDraw(bg, x: int, y: int):
+    bg.fill(idef.COLOR_WHITE)
+    for i in range(11):
+        for j in range(len(nowMap[i])):
+            
+            
+
+#--------------------------------------------------
+# Map Data Load function
+#--------------------------------------------------
+def MapLoad(_map: int):
+    ret = [[]]
+    
+    # map 
+    if _map == 0:
+        ret.append([1,1,1,1,1,1,1,1])
+        ret.append([1,1,1,1,1,1,1,1])
+        ret.append([1,1,1,1,1,1,1,1])
+        ret.append([1,1,1,1,1,1,1,1])
+        ret.append([1,1,1,1,1,1,1,1])
+        ret.append([1,1,1,1,1,1,1,1])
+        ret.append([1,1,1,1,1,1,1,1])
+        ret.append([1,1,1,1,1,1,1,1])
+
+    elif _map == 1:
+        ret.append([0,0,0,0,0,0,0,0])
+        ret.append([0,0,0,0,0,0,0,0])
+        ret.append([0,0,0,0,0,0,0,0])
+        ret.append([0,0,0,0,0,0,0,0])
+        ret.append([0,0,0,0,0,0,0,0])
+        ret.append([0,0,0,0,0,0,0,0])
+        ret.append([0,0,0,0,0,0,0,0])
+        ret.append([0,0,0,0,0,0,0,0])
+
+    return ret
 
 #--------------------------------------------------
 # player direction image read function
@@ -115,53 +226,44 @@ def MapMain(bg, clk):
         # game state update
         key = pygame.key.get_pressed()
         timer = timer + 1
-        
         bg.fill(idef.COLOR_WHITE)
-
+        
         # 左キー入力
         if key[pygame.locals.K_LEFT] == 1:
-            if(pygame.mixer.get_busy() == False):
-                print("left")
+            if pygame.mixer.get_busy() == False:
+                #print("left")
                 posX = posX - 1
                 CharDraw(bg, DIR_L)
                 print(str(posX) + ", " + str(posY))
                 soundWalk.play()
-            else:
-                pygame.event.clear()
         
         # 右キー入力
         if key[pygame.locals.K_RIGHT] == 1:
-            if(pygame.mixer.get_busy() == False):
-                print("right")
+            if pygame.mixer.get_busy() == False:
+                #print("right")
                 posX = posX + 1
                 CharDraw(bg, DIR_R)
                 print(str(posX) + ", " + str(posY))
                 soundWalk.play()
-            else:
-                pygame.event.clear()
 
         # 上キー入力
         if key[pygame.locals.K_UP] == 1:
-            if(pygame.mixer.get_busy() == False):
-                print("up")
+            if pygame.mixer.get_busy() == False:
+                #print("up")
                 posY = posY - 1
                 CharDraw(bg, DIR_U)
                 print(str(posX) + ", " + str(posY))
                 soundWalk.play()
-            else:
-                pygame.event.clear()
 
         # 下キー入力
         if key[pygame.locals.K_DOWN] == 1:
-            if(pygame.mixer.get_busy() == False):
-                print("down")
+            if pygame.mixer.get_busy() == False:
+                #print("down")
                 posY = posY + 1
                 CharDraw(bg, DIR_D)
                 print(str(posX) + ", " + str(posY))
                 soundWalk.play()
-            else:
-                pygame.event.clear()
-
+        
         bg.blit(charImg,[idef.WINDOW_WIDTH/2 -32, idef.WINDOW_HEIGHT/2 -32])
         pygame.display.update()
         clk.tick(20)
